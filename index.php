b5753a99c967ebc4515a92b16abc86705d194d39
Arjan de Ruijter <rra@mboutrecht.nl>
2016-05-09 10:09:25 +0200

    Dit zijn de eerste wijzigingen in index.php en style.css

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
-<?php
-    echo "Hallo less wereld";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+       <title>LESS</title>       
+    </head>
+    <body>
+        <h3>LESS tutorial</h3>
+
+        <ul>
+            <li><a href="intro_less.php">Introductie en setup Less</a></li>
+            
+        </ul>
+    </body>
+</html>
