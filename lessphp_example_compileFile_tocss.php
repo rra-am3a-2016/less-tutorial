53b8eaab8f80bfc89b05ba80234f264db12b6445
Arjan de Ruijter <rra@mboutrecht.nl>
2016-05-18 15:08:16 +0200

    Tabel less

--- lessphp_example_compileFile_tocss.php
+++ lessphp_example_compileFile_tocss.php
@@ -4,12 +4,19 @@
     $less = new lessc();
     
     $colorArray = array( "changeColor1" => "rgb(67,103,188)",
-                         "changeColor2" => "rgb(10, 18, 180)");
+                         "changeColor2" => "rgb(10, 18, 180)",
+                         "lineThicknessSolid" => "1px solid",
+                         "textColor" => "lighten(#FF0000, 0%)");
                          
     if (isset( $_POST["submit"]))
     {
         var_dump($_POST);
         $colorArray["changeColor1"] = $_POST["color1"];
+        $colorArray["changeColor2"] = $_POST["color2"];
+        $colorArray["lineThicknessSolid"] = $_POST["lineThickness"]."px solid";
+        $colorArray["textColor"] = $_POST["darkOrLight"]."(".
+                                   $_POST["color3"].", ".$_POST["colorPerc"]."% )";
+        
     }
     
     $less->setVariables($colorArray);
@@ -123,7 +130,36 @@
             <table>
                 <tr><td>Voer kleur 1 in: </td><td><input type="color" name="color1" value="#FFAA00"></td></tr>
                 <tr><td>Voer kleur 2 in: </td><td><input type="color" name="color2" value="#FFAA00"></td></tr>
-                <tr><td></td><td><input type="submit" name="submit" value="Nieuwe kleur"></td></tr>
+                <tr><td>Kies een lijndikte: </td><td>
+                                                    <select name="lineThickness">
+                                                        <option value="1">1</option>
+                                                        <option value="5">5</option>
+                                                        <option value="10">10</option>
+                                                        <option value="15">15</option>                                                        
+                                                        <option value="20">20</option>                                                       
+                                                    </select>
+                                                 </td>
+                </tr>
+                <tr><td>Voer tekstkleur in: </td><td><select name="darkOrLight">
+                                                        <option value="darken">darken</option>
+                                                        <option value="lighten">lighten</option>                                             
+                                                    </select>
+                                                    <input type="color" name="color3" value="#FFAA00">
+                                                    <select name="colorPerc">
+                                                        <option value="1">1</option>
+                                                        <option value="10">10</option>
+                                                        <option value="20">20</option>
+                                                        <option value="30">30</option>                                                        
+                                                        <option value="40">40</option>                                                       
+                                                    </select>
+                                             </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>               
+                        <input type="submit" name="submit" value="verander tabel!"> 
+                    </td>
+                </tr>             
             </table>     
         </form>
     </body>
