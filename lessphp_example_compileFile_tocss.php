298b63c31ce2d2cc2ed1fa8acdf1add63ad83815
Arjan de Ruijter <rra@mboutrecht.nl>
2016-05-19 13:16:00 +0200

    less

--- lessphp_example_compileFile_tocss.php
+++ lessphp_example_compileFile_tocss.php
@@ -6,19 +6,32 @@
     $colorArray = array( "changeColor1" => "rgb(67,103,188)",
                          "changeColor2" => "rgb(10, 18, 180)",
                          "lineThicknessSolid" => "1px solid",
-                         "textColor" => "lighten(#FF0000, 0%)");
+                         "textColorTd" => "lighten(#121212, 0%)",
+                         "textColorTh" => "lighten(#232323, 0%)",
+                         "color1" => "#FFAA00",
+                         "color2" => "#FFAA00",
+                         "color3" => "#FFAA00",
+                         "color4" => "#FFAA00"
+                         );
                          
     if (isset( $_POST["submit"]))
     {
         var_dump($_POST);
+        
         $colorArray["changeColor1"] = $_POST["color1"];
         $colorArray["changeColor2"] = $_POST["color2"];
         $colorArray["lineThicknessSolid"] = $_POST["lineThickness"]."px solid";
-        $colorArray["textColor"] = $_POST["darkOrLight"]."(".
-                                   $_POST["color3"].", ".$_POST["colorPerc"]."% )";
+        $colorArray["textColorTd"] = $_POST["darkOrLightTd"]."(".
+                                   $_POST["color3"].", ".$_POST["colorPercTd"]."% )";
+        $colorArray["textColorTh"] = $_POST["darkOrLightTh"]."(".
+                                   $_POST["color4"].", ".$_POST["colorPercTh"]."% )";
+        $colorArray["color1"] = $_POST["color1"];
+        $colorArray["color2"] = $_POST["color2"];
+        $colorArray["color3"] = $_POST["color3"];
+        $colorArray["color4"] = $_POST["color4"];
         
     }
-    
+    var_dump($colorArray);
     $less->setVariables($colorArray);
     
     $less->compileFile("less_compilefile.less", "less_compilefile.css");
@@ -128,8 +141,8 @@
         
         <form action="./lessphp_example_compileFile_tocss.php" method="post">
             <table>
-                <tr><td>Voer kleur 1 in: </td><td><input type="color" name="color1" value="#FFAA00"></td></tr>
-                <tr><td>Voer kleur 2 in: </td><td><input type="color" name="color2" value="#FFAA00"></td></tr>
+                <tr><td>Voer kleur 1 in: </td><td><input type="color" name="color1" value="<?php echo $colorArray['color1']; ?>"></td></tr>
+                <tr><td>Voer kleur 2 in: </td><td><input type="color" name="color2" value="<?php echo $colorArray['color2']; ?>"></td></tr>
                 <tr><td>Kies een lijndikte: </td><td>
                                                     <select name="lineThickness">
                                                         <option value="1">1</option>
@@ -140,18 +153,32 @@
                                                     </select>
                                                  </td>
                 </tr>
-                <tr><td>Voer tekstkleur in: </td><td><select name="darkOrLight">
-                                                        <option value="darken">darken</option>
-                                                        <option value="lighten">lighten</option>                                             
-                                                    </select>
-                                                    <input type="color" name="color3" value="#FFAA00">
-                                                    <select name="colorPerc">
-                                                        <option value="1">1</option>
-                                                        <option value="10">10</option>
-                                                        <option value="20">20</option>
-                                                        <option value="30">30</option>                                                        
-                                                        <option value="40">40</option>                                                       
-                                                    </select>
+                <tr><td>Voer tekstkleur in voor de vakjes: </td><td><select name="darkOrLightTd">
+                                                                        <option value="darken">darken</option>
+                                                                        <option value="lighten">lighten</option>                                             
+                                                                    </select>
+                                                                    <input type="color" name="color3" value="<?php echo $colorArray['color3']; ?>" >
+                                                                    <select name="colorPercTd">
+                                                                        <option value="1">1</option>
+                                                                        <option value="10">10</option>
+                                                                        <option value="20">20</option>
+                                                                        <option value="30">30</option>                                                        
+                                                                        <option value="40">40</option>                                                       
+                                                                    </select>
+                                             </td>
+                </tr>
+                <tr><td>Voer tekstkleur in voor de kopregel: </td><td><select name="darkOrLightTh">
+                                                                        <option value="darken">darken</option>
+                                                                        <option value="lighten">lighten</option>                                             
+                                                                      </select>
+                                                                      <input type="color" name="color4" value="<?php echo $colorArray['color4']; ?>">
+                                                                      <select name="colorPercTh">
+                                                                        <option value="1">1</option>
+                                                                        <option value="10">10</option>
+                                                                        <option value="20">20</option>
+                                                                        <option value="30">30</option>                                                        
+                                                                        <option value="40">40</option>                                                       
+                                                                      </select>
                                              </td>
                 </tr>
                 <tr>
